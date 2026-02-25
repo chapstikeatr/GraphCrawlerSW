535cf3e50185fa703b5c235a6b4f04317efc4f25
Carter Lee Stickle <cstickl3@gal-i9.charlotte.edu>
2026-02-24 21:45:55 -0500

    im stupid

--- bench_graphcrawler.sh
+++ bench_graphcrawler.sh
@@ -9,6 +9,12 @@
 srun $HOME/GraphCrawlerSW/sequential/seqLC "Tom Hanks" 2
 srun $HOME/GraphCrawlerSW/sequential/seqLC "Tom Hanks" 3
 srun $HOME/GraphCrawlerSW/sequential/seqLC "Tom Hanks" 4
-srun $HOME/GraphCrawlerSW/Parallel/paraLC"Tom Hanks" 2
-srun $HOME/GraphCrawlerSW/Parallel/paraLC"Tom Hanks" 3
-srun $HOME/GraphCrawlerSW/Parallel/paraLC"Tom Hanks" 4
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 2 8
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 3 8
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 4 8
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 2 20
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 3 20
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 4 20
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 2 50
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 3 50
+srun $HOME/GraphCrawlerSW/Parallel/paraLC "Tom Hanks" 4 50
